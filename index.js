19a0af59908332854ed82f433ba71cff95b0a8b7
adamf <adam.freeman@sli-systems.com>
2019-01-29 16:47:34 +1300

    Improved basic example

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const https = require('https'),
-  apiBase = 'churchmetrics.com',
-  credentials = require('./credentials.json');
+      path = require('path'),
+      prettyjson = require('prettyjson');
+
+const apiBase = 'churchmetrics.com',
+      credentials = require('./credentials.json');
+
+const pageSize = 5,
+      dataDir = path.resolve(__dirname, 'data/');
 
 console.log(`Email is: ${credentials.email}`);
 
@@ -13,13 +19,17 @@ var options = {
   }
 };
 
-function doQuery(path) {
+async function writeObjectToFile(obj, fileName) {
+  console.log(path.resolve(dataDir, fileName));
+}
+
+async function doQuery(path) {
   options.path = path;
 
   return new Promise((resolve, reject) => {
     https.request(options, function(res) {
-      // console.log('STATUS: ' + res.statusCode);
-      // console.log('HEADERS: ' + JSON.stringify(res.headers));
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + prettyjson.render(res.headers));
       res.setEncoding('utf8');
       res.on('data', function(chunk) {
         resolve(chunk);
@@ -28,6 +38,16 @@ function doQuery(path) {
   });
 }
 
-doQuery('/api/v1/users.json').then((msg) => {
-  console.log(msg);
-});
+async function main() {
+  let msg = await doQuery('/api/v1/users.json')
+  console.log(prettyjson.render(JSON.parse(msg)));
+  // console.log(dataDir);
+  await writeObjectToFile(msg, 'users.json');
+
+
+  // doQuery(`/api/v1/records.json?per_page=${pageSize}`).then((msg) => {
+  //   console.log(prettyjson.render(JSON.parse(msg)));
+  // });
+}
+
+main();
